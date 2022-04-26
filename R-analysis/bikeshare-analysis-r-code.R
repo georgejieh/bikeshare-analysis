8a07290432c28db8a9defff4c68c77c2c8582ac4
George Jieh <george.jieh@gmail.com>
2022-04-26 13:06:13 -0700

    create bikeshare-analysis-r-code.R. Import and merge data into R code

---
+++ R-analysis/bikeshare-analysis-r-code.R
@@ -0,0 +1,9 @@
+# import data
+setwd("/Users/nancy/Documents/Git Repo/Google Case Studies/bikeshare-analysis/processed-data/csv-files-for-import")
+getwd()
+data1 <- read.csv("ride_types.csv", stringsAsFactors = T)
+data2 <- read.csv("ride_times.csv", stringsAsFactors = T)
+data3 <- merge(data1, data2, by.x = "ride_id", by.y = "ride_id")
+head(data3)
+str(data3)
+summary(data3)
