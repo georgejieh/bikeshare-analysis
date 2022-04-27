45b1db2e3d1458abe3328649ad6290869f1df192
George Jieh <george.jieh@gmail.com>
2022-04-27 15:22:36 -0700

    update data frame variable name

--- R-analysis/bikeshare-analysis-r-code.R
+++ R-analysis/bikeshare-analysis-r-code.R
@@ -3,16 +3,16 @@ setwd("/Users/nancy/Documents/Git Repo/Google Case Studies/bikeshare-analysis/pr
 getwd()
 data1 <- read.csv("ride_types.csv", stringsAsFactors = T)
 data2 <- read.csv("ride_times.csv", stringsAsFactors = T)
-data3 <- merge(data1, data2, by.x = "ride_id", by.y = "ride_id")
-head(data3)
-str(data3)
-summary(data3)
+data <- merge(data1, data2, by.x = "ride_id", by.y = "ride_id")
+head(data)
+str(data)
+summary(data)
 
 #delete useless data
-data3$ride_id <- NULL
+data$ride_id <- NULL
+rm(data1, data2)
 
 # install and activate ggplot2
 install.packages("ggplot2")
 library(ggplot2)
-
 
