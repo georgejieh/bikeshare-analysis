491de0f9eb8b47bb33608ef36185260451c82dbb
George Jieh <george.jieh@gmail.com>
2022-04-27 22:49:42 -0700

    create prelim bar charts, set factors to new columns, rename levels

--- R-analysis/bikeshare-analysis-r-code.R
+++ R-analysis/bikeshare-analysis-r-code.R
@@ -8,11 +8,36 @@ head(data)
 str(data)
 summary(data)
 
-#delete useless data
+# delete useless data
 data$ride_id <- NULL
 rm(data1, data2)
+
+# set factors for some integer columns
+data$day_of_week <- as.factor(data$day_of_week)
+data$month_of_year <- as.factor(data$month_of_year)
+
+# rename factor levels
+levels(data$day_of_week) <- c("Sun", "Mon", "Tue", "Wed", 
+                              "Thu", "Fri", "Sat")
+levels(data$month_of_year) <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 
 # install and activate ggplot2
 install.packages("ggplot2")
 library(ggplot2)
 
+# bar charts
+bar_base <- ggplot(data=data, aes(x=member_casual))
+geom_bar_base <- geom_bar(stat="count", colour="black", aes(fill=category))
+
+category <- data$season
+bar_base + geom_bar_base
+
+category <- data$day_of_week
+bar_base + geom_bar_base
+
+category <- data$rideable_type
+bar_base + geom_bar_base
+
+category <- data$month_of_year
+bar_base + geom_bar_base
