b279d71da206ef86be197178ed5f2b3cc389c983
George Jieh <george.jieh@gmail.com>
2022-05-27 19:59:48 -0700

    redact file path from R and SQL codes

--- R-analysis/bikeshare-analysis-r-code.R
+++ R-analysis/bikeshare-analysis-r-code.R
@@ -3,7 +3,7 @@ install.packages("ggplot2")
 library(ggplot2)
 
 # import data
-setwd("/Users/nancy/Documents/Git Repo/Google Case Studies/bikeshare-analysis/processed-data/csv-files-for-import")
+setwd("/.../bikeshare-analysis/processed-data/csv-files-for-import")
 getwd()
 data1 <- read.csv("ride_types.csv", stringsAsFactors = T)
 data2 <- read.csv("ride_times.csv", stringsAsFactors = T)
@@ -21,7 +21,7 @@ data$day_of_week <- as.factor(data$day_of_week)
 data$month_of_year <- as.factor(data$month_of_year)
 
 # rename factor levels
-levels(data$day_of_week) <- c("Sun", "Mon", "Tue", "Wed", 
+levels(data$day_of_week) <- c("Sun", "Mon", "Tue", "Wed",
                               "Thu", "Fri", "Sat")
 levels(data$month_of_year) <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
                                 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
@@ -31,35 +31,35 @@ levels(data$rideable_type) <- c("Classic", "Docked", "Electric")
 data$ride_length <- data$ride_length/60
 
 # box plots
-boxplot_base <- ggplot(data=data, aes(y=ride_length, 
+boxplot_base <- ggplot(data=data, aes(y=ride_length,
                                       colour=member_casual))
 
-boxplot1 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=season)) + 
+boxplot1 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=season)) +
   coord_cartesian(ylim=c(0,85))
 
-boxplot2 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=day_of_week)) + 
+boxplot2 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=day_of_week)) +
   coord_cartesian(ylim=c(0,85))
 
-boxplot3 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=month_of_year)) + 
+boxplot3 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=month_of_year)) +
   coord_cartesian(ylim=c(0,100))
 
-boxplot4 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=rideable_type)) + 
+boxplot4 <- boxplot_base + geom_boxplot(size=1.10, outlier.shape = NA, aes(x=rideable_type)) +
   coord_cartesian(ylim=c(0,115))
 
 # histograms
 histogram_base <- ggplot(data=data, aes(x=ride_length, fill=member_casual))
 
-histogram1 <- histogram_base + geom_histogram(binwidth=2, colour="white", alpha=0.65) + 
+histogram1 <- histogram_base + geom_histogram(binwidth=2, colour="white", alpha=0.65) +
   geom_freqpoly(binwidth=2, aes(colour=rideable_type)) +
   facet_grid(month_of_year~member_casual, scales="free") +
   coord_cartesian(xlim=c(0,35))
- 
-histogram2 <- histogram_base + geom_histogram(binwidth=2, colour="white", alpha=0.65) + 
+
+histogram2 <- histogram_base + geom_histogram(binwidth=2, colour="white", alpha=0.65) +
   geom_freqpoly(binwidth=2, aes(colour=rideable_type)) +
   facet_grid(season~member_casual, scales="free") +
   coord_cartesian(xlim=c(0,40))
 
-histogram3 <- histogram_base + geom_histogram(binwidth=2, colour="white", alpha=0.65) + 
+histogram3 <- histogram_base + geom_histogram(binwidth=2, colour="white", alpha=0.65) +
   geom_freqpoly(binwidth=2, aes(colour=rideable_type)) +
   facet_grid(day_of_week~member_casual, scales="free") +
   coord_cartesian(xlim=c(0,35))
--- psql-queries/csv_import.sql
+++ psql-queries/csv_import.sql
@@ -1,8 +1,8 @@
-COPY ride_types FROM 
-'/Users/nancy/Documents/Git Repo/Google Case Studies/bikeshare-analysis/processed-data/csv-files-for-import/ride_types.csv' CSV HEADER;
+COPY ride_types FROM
+'/.../bikeshare-analysis/processed-data/csv-files-for-import/ride_types.csv' CSV HEADER;
 
-COPY ride_times FROM 
-'/Users/nancy/Documents/Git Repo/Google Case Studies/bikeshare-analysis/processed-data/csv-files-for-import/ride_times.csv' CSV HEADER;
+COPY ride_times FROM
+'/.../bikeshare-analysis/processed-data/csv-files-for-import/ride_times.csv' CSV HEADER;
 
-COPY ride_dates FROM 
-'/Users/nancy/Documents/Git Repo/Google Case Studies/bikeshare-analysis/processed-data/csv-files-for-import/ride_dates.csv' CSV HEADER;
+COPY ride_dates FROM
+'/.../bikeshare-analysis/processed-data/csv-files-for-import/ride_dates.csv' CSV HEADER;
